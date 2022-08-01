5d8ab2be4f12a63a4189d543ae4c1addb830bde2
mutharasi <rithikakumar01@gmail.com>
2022-08-01 23:27:58 +0530

    js added

---
+++ script.js
@@ -0,0 +1,16 @@
+const list=document.querySelector('.task')
+const addButton=document.querySelector('.button')
+
+function display() {
+    var inputValue = document.querySelector('#inputBox')
+    if (!inputValue.value.length==0) {    
+        list.innerHTML +=`<div class="lists">
+        ${inputValue.value}
+        <button class=delete onclick="deletes(this)"><i class="fa fa-trash-o"></i></button>
+        </div>`            
+    }
+    else{
+        alert('enter the task')
+    }
+    inputValue.value=''
+}
