4fb20878c929d7bf23988b419c723235d8e6bab2
mutharasi <rithikakumar01@gmail.com>
2022-08-03 18:31:06 +0530

    edit option added

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 const list = document.querySelector('.task')
-const addButton = document.querySelector('.button')
-function display() {
-    var inputValue = document.querySelector('#inputBox')
+const addButton = document.querySelector('.addButton')
+const inputValue = document.querySelector('#inputBox')
+const myform = document.querySelector('.input')
+function display(e) {
+    e.preventDefault()
     if (!inputValue.value.length == 0) {
         list.innerHTML += `<div class="lists">
+
         ${inputValue.value}
-        <button class=delete onclick="deletes(this)"><i class="fa fa-trash-o"></i></button>
+        <div class="iconBtn">
+        <button class="icons" onclick="edit(this)"><i class="fa fa-edit"></i></button>
+        
+        <button class="icons" onclick="deletes(this)"><i class="fa fa-trash-o"></i></button>
+        </div>
         </div>`
     }
     else {
@@ -13,7 +20,13 @@ function display() {
     }
     inputValue.value = ''
 }
-function deletes(x)
-{
-    x.parentNode.remove()
+function deletes(x) {
+    x.parentElement.parentElement.remove()
 }
+function edit(x) {
+    let edited = x.parentElement.parentElement.innerText;
+    inputValue.value = edited;
+    x.parentElement.parentElement.remove();
+}
+myform.addEventListener('submit', display)
+
