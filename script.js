5cf3f622f094892dbcc438f8141f449991fdd104
mutharasi <rithikakumar01@gmail.com>
2022-08-03 23:43:19 +0530

    edit option added

--- script.js
+++ script.js
@@ -2,31 +2,61 @@ const list = document.querySelector('.task')
 const addButton = document.querySelector('.addButton')
 const inputValue = document.querySelector('#inputBox')
 const myform = document.querySelector('.input')
+const taskArray=[];
+
+function addTask(userValue){
+    taskArray.push(userValue)
+    list.innerHTML += `<div class="lists">
+
+    ${userValue}
+    <div class="iconBtn">
+    <button class="icons" onclick="edit(this)"><i class="fa fa-edit"></i></button>
+    
+    <button class="icons" onclick="deletes(this)"><i class="fa fa-trash-o"></i></button>
+    </div>
+    </div>`
+}
+
+function setItems(){
+ localStorage.setItem("tasks",JSON.stringify(taskArray))
+}
+
+function getItems(){
+    let tasks=localStorage.getItem('tasks')
+    if(!tasks)
+    return
+
+    tasks=JSON.parse(tasks)
+    for(index in tasks)
+    {
+        addTask(tasks[index])
+    }
+}
+getItems()
 function display(e) {
     e.preventDefault()
-    if (!inputValue.value.length == 0) {
-        list.innerHTML += `<div class="lists">
-
-        ${inputValue.value}
-        <div class="iconBtn">
-        <button class="icons" onclick="edit(this)"><i class="fa fa-edit"></i></button>
-        
-        <button class="icons" onclick="deletes(this)"><i class="fa fa-trash-o"></i></button>
-        </div>
-        </div>`
+    const userValue=inputValue.value
+    const length=userValue.length
+    if (length == 0) {
+        alert('enter the task')
     }
     else {
-        alert('enter the task')
+        addTask(userValue);
+        setItems()
     }
     inputValue.value = ''
 }
+// ------------------------------------------------------
 function deletes(x) {
-    x.parentElement.parentElement.remove()
+ x.parentElement.parentElement.remove()
 }
+
 function edit(x) {
     let edited = x.parentElement.parentElement.innerText;
     inputValue.value = edited;
     x.parentElement.parentElement.remove();
+    setItems()
 }
+
 myform.addEventListener('submit', display)
 
